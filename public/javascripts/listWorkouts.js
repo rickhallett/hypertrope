2139d6ad1f9135d5e4222b49c66a9c0ce47ebea0
rickhallett <richard.hallett@ims-evolve.com>
2018-12-26 13:20:22 +0000

    listWorkout.js trims unnecessary date data

---
+++ public/javascripts/listWorkouts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const trimDate = (date) => date.innerText.substring(0,15) 
+
+    const dateHTML = document.getElementsByClassName('date');
+    for(let i = 0; i < dateHTML.length; i++) {
+        let currentDate = dateHTML[i];
+        currentDate.innerText = trimDate(currentDate);
+    }
+});
