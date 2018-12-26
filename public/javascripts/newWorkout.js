c57add9dc2aca7dc030c78c7d4f191ca8c0b7e46
rickhallett <richard.hallett@ims-evolve.com>
2018-12-26 18:56:46 +0000

    use javascript reload function to re-execture beauter.js after changing DOM

--- public/javascripts/newWorkout.js
+++ public/javascripts/newWorkout.js
@@ -1,5 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('exercise-selectors');
-    console.log(form)
+
+    const reloadBeauterJS = () => {
+        let head = document.getElementsByTagName('head')[0];
+        let beauterJS = document.createElement('script');
+        beauterJS.src = `https://rawgit.com/outboxcraft/beauter/master/beauter.min.js?cachebuster=${new Date().getTime()}`;
+        head.appendChild(beauterJS);
+    }
+
+    $('#submit-workout')
+        .on('click', function(event) {
+            event.preventDefault();
+        });
+
+    let form = $('#exercise-selectors');
+    let accordian = $('#accordian-button');
+    let panel = $('#accordian-panel');
+
+    const selectedNumber = $('#select-number-exercise').val();
+
+    form.html('');
+
+    for(let i = 1; i <= /*selectedNumber*/ 3; i++) {
+        let newAccordian = accordian.clone()
+        let newPanel = panel.clone();
+
+        console.log(newAccordian);
+
+        newAccordian.html(`Exercise ${i}`)
+        
+        form.append(newAccordian);
+        form.append(newPanel);
+    }
+
+    debugger;reloadBeauterJS();
 });
+
 
