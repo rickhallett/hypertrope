24d1d9bb1e69767a30d89bcd2929e661681a3de3
rickhallett <richard.hallett@ims-evolve.com>
2018-12-27 00:23:18 +0000

    a

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const constants = require('../data/constants');
-const utilities = require('../public/javascripts/utilities');
+// const utilities = require('../public/javascripts/utilities');
 
 /**
  * RESTFUL ROUTING
