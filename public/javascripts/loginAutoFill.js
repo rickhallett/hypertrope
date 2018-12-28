98f334dc60c18a6d8be83a2b97f54a17a13b68b6
rickhallett <richard.hallett@ims-evolve.com>
2018-12-28 07:29:58 +0000

    loginAutoFill.js

---
+++ public/javascripts/loginAutoFill.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function() {
+// });
+
+const login = () => {
+    const loginName = document.getElementById('login_name');
+    const password = document.getElementById('pword_input_1');
+    const loginButton = document.getElementById('login');
+
+    loginName.value = 'Rick';
+    password.value = 'admin';
+    loginButton.click();
+}
