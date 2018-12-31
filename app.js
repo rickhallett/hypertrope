2e433851123485647b39586280aea816c0e029e2
rickhallett <richard.hallett@ims-evolve.com>
2018-12-31 01:09:58 +0000

    added winston logs for detailed logging

--- app.js
+++ app.js
@@ -1,70 +1,88 @@
 /**
  * DEPENDENCEIS
  */
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const path = require('path');
-const createError = require('http-errors');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const flash = require('connect-flash');
+const fs = require("fs");
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const path = require("path");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
 
 const options = { useNewUrlParser: true };
-const nodeUtils = require('./utils/nodeUtils');
-const consoleColors = require('./data/consoleColors');
+const nodeUtils = require("./utils/nodeUtils");
+const morgan = require("morgan");
+const winston = require('./config/winston');
+const colors = require('colors');
 
-// const indexRouter = require('./routes/index');
-// const authRouter = require('./routes/auth');
-// const usersRouter = require('./routes/users');
-// const workoutRouter = require('./routes/workouts')
-const godRouter = require('./routes/routes');
+const godRouter = require("./routes/routes");
 
 const app = express();
-// app.set('env', 'development');
-app.set('env', 'production');
+app.set("env", "development");
+// app.set('env', 'production');
 
 let config;
-if(app.get('env') === 'development') config = require('./secret');
-const consoleSpacer = '\n\n=======================================================================\n\nServer Console Output:\n';
+if (app.get("env") === "development") config = require("./secret");
+const consoleSpacer =
+  "\n\n=======================================================================\n\nServer Console Output:\n";
 
 /**
  * DATABASE
  */
-const mongoURI = app.get('env') === 'development' ? config.dev_mongoURI : process.env.MONGO_URI;
-mongoose.connect(mongoURI, options);
+const mongoURI =
+  app.get("env") === "development"
+    ? config.dev_mongoURI
+    : process.env.MONGO_URI;
+mongoose.connect(
+  mongoURI,
+  options
+);
 const db = mongoose.connection;
-db.on('error', console.error.bind(this, `Error connecting to database ${this.name}`));
-db.once('open', function() {
-  console.log('\x1b[33m%s\x1b[0m', `Database: ${this.name} connected successfully on port ${this.port} @host ${this.host}${consoleSpacer}`);
+db.on(
+  "error",
+  console.error.bind(this, `Error connecting to database ${this.name}`)
+);
+db.once("open", function() {
+  console.log(
+    "\x1b[33m%s\x1b[0m",
+    `Database: ${this.name} connected successfully on port ${this.port} @host ${
+      this.host
+    }${consoleSpacer}`
+  );
 });
 
 /**
  * VIEW ENGINE
  */
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 /**
  * MIDDLEWARE
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser(config.sessionSecret));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan('combined', { stream: winston.stream }));
 /**
  * AUTH MIDDLEWARE
  */
-app.use(require('express-session')({
-  secret: app.get('env') === 'development' ? config.sessionSecret : process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}));
+app.use(
+  require("express-session")({
+    secret:
+      app.get("env") === "development"
+        ? config.sessionSecret
+        : process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -74,10 +92,20 @@ app.use(flash());
 /**
  * PASSPORT CONFIG
  */
-const Account = require('./models/Account');
+const Account = require("./models/Account");
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
+
+
+/**
+ * WINSTON CONSOLE LOGGER (must be before router)
+ * success logger
+ * info logger
+ * error logger
+ * trace/silly logger
+ */
+
 
 
 /**
@@ -87,7 +115,14 @@ passport.deserializeUser(Account.deserializeUser());
 // app.use('/auth', authRouter);
 // app.use('/users', usersRouter);
 // app.use('/workouts', workoutRouter);
-app.use('/', godRouter);
+app.use("/", godRouter);
+
+/**
+ * WINSTON ERROR LOGGER (must be after router)
+ */
+
+
+
 
 /**
  * ERROR HANDLING
@@ -101,29 +136,41 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 /**
  * INITIALISE SERVER
  */
 //Get port from environment and store in Express.
-const port = (process.env.PORT || '3000');
-app.set('port', port);
-
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
 // Create HTTP server.
 const server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces.
 server.listen(port, function(err) {
-  if(!err) {
-    console.log(`Server listening on ${port}`);
+  if (!err) {
+    winston.debug(`Server listening on ${port}`);
   }
 });
+
+const t = {
+  fds: [1, 2, 3, [1, 2, {name: 'ksksks'}]],
+  fdds: {
+    fdd: {
+      sjssj: {
+        name: 'skdfksdfksdf'
+      }
+    }
+  }
+}
 
 module.exports = app;
---
+++ config/winston.js
@@ -0,0 +1,48 @@
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const appRoot = require('app-root-path');
+
+const options = {
+  file: {
+    filename: `${appRoot.path}/app.log`,
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.prettyPrint()
+    ),
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: true,
+    prettyPrint: true
+  },
+  error: {
+    filename: `${appRoot.path}/error.log`,
+    level: 'error',
+    format: winston.format.combine(
+      winston.format.splat(),
+      winston.format.prettyPrint()
+    ),
+    handleExceptions: true,
+    json: true,
+    colorize: true,
+    prettyPrint: true
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.File(options.error),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
