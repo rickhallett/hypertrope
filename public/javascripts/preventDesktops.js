9cf0258c26991d14d03676a1071374d0d4565ed7
rickhallett <richard.hallett@ims-evolve.com>
2018-12-31 13:21:22 +0000

    desktop restriction on all pages in layout scripts

--- public/javascripts/preventDesktops.js
+++ public/javascripts/preventDesktops.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if (document.body.clientWidth > 414) {
+  if (document.body.clientWidth > 414 && window.location.pathname !== '/preventDesktops') {
     window.location.href = '/preventDesktops';
   }
 });
