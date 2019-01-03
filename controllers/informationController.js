74c88e24531cd3d8f9a7093706f2af50d908e5cf
rickhallett <richard.hallett@ims-evolve.com>
2019-01-03 13:56:36 +0000

    remove unsused informationController requires as crashing node

--- controllers/informationController.js
+++ controllers/informationController.js
@@ -1,17 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const Account = require("../models/Account");
-
-const router = express.Router();
-
-const Workout = require("../models/Workout");
-const newWorkout = require("../models/newWorkout");
-const constants = require("../data/constants");
-
-const utilities = require("../public/javascripts/utilities");
-const nodeUtils = require("../utils/nodeUtils");
-const customMiddleware = require('../controllers/customMiddlewareController');
 
 /**
  * INFORMATION CONTROLLER
