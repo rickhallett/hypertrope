1e5e4c3d5cec2954dae944fb5a6eaa0fb9c1b87b
rickhallett <richard.hallett@ims-evolve.com>
2019-01-03 23:06:48 +0000

    added data fields and modfified calculateWork() to automatically double the sets from dummbells so as to accurately reflect workload

--- models/Workout.js
+++ models/Workout.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const exerciseData = require("../data/exercises.json").exercises;
 
 const ExerciseSchema = new Schema({
   name: { type: String, default: "n/a" },
@@ -15,12 +16,43 @@ const WorkoutSchema = new Schema({
   comments: String
 });
 
+var inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 }
+];
+
+function isCherries(fruit) {
+  return fruit.name === "cherries";
+}
+
+console.log(inventory.find(isCherries));
+// { name: 'cherries', quantity: 5 }
+
 WorkoutSchema.methods.calculateWork = function() {
   let work = 0;
 
+  // exerciseName = (el, i, arr) => {
+  //   return exerciseData[name] === name;
+  // }
+
+  isDumbbellExercise = name => {
+    let isDb = false;
+    exerciseData.forEach(el => {
+      if (el.value === name) {
+        isDb = el.db;
+      }
+    });
+    return isDb;
+  };
+
   this.exercises.forEach(exercise => {
     if (exercise.sets !== undefined && exercise.reps !== undefined) {
-      work += exercise.sets * exercise.reps * exercise.weight;
+      if (isDumbbellExercise(exercise.name)) {
+        work += exercise.sets * 2 * exercise.reps * exercise.weight;
+      } else {
+        work += exercise.sets * exercise.reps * exercise.weight;
+      }
     }
   });
 
