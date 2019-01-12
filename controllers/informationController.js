b90160044b989d63bbc1d55a9d9181ff1d1d01fe
rickhallett <richard.hallett@ims-evolve.com>
2019-01-12 14:33:30 +0000

    prettierfied all controllers and created central index exporter

--- controllers/informationController.js
+++ controllers/informationController.js
@@ -1,13 +1,11 @@
-const express = require("express");
-
 /**
  * INFORMATION CONTROLLER
  */
 
 const informationController = {};
 
-informationController.getInformation = function(req, res) {
-  res.render("information");
+informationController.getInformation = (req, res) => {
+    res.render('information');
 };
 
 module.exports = informationController;
